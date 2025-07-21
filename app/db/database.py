87f3196d0f206f091b949d8461ea419be7bec89a
Akbarjon001 <Akbarjonsolihov1@gmail.com>
2025-07-21 19:11:15 +0900

    Update For 21.07.2025-19-11

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,5 @@
 from typing import AsyncGenerator
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
